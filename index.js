1e5210e22a307f4c2724695f49fd484647c2890e
Nam Vu <superbigsoft@gmail.com>
2020-04-25 00:01:22 +1000

    Support multiple base folders.

--- index.js
+++ index.js
@@ -27,31 +27,33 @@ class TemplateCachePlugin {
           (cb) => {
             var filelist = 'angular.module("' + this.options.moduleName + '").run(["$templateCache",function($templateCache){"use strict";';
 
-            var files = glob.sync(this.options.baseFolder + "**/*.html", {});
-
-            for (var filename of files) {
-              if (filename.substr(-4) === 'html') {
-                let fullpath = path.resolve(filename);
-                // adds this file to being watched by webpack for a rebuild
-                compilation.fileDependencies.add(fullpath);
-                var source = fs.readFileSync(filename);
-                source = source.toString();
-                source = htmlMinifier.minify(source, Object.assign(defaultMinifyOptions, this.options.minifyOptions));
-                source = source.replace(/\r?\n|\r/g, "");
-                source = source.replace(/\\/g, "\\\\");
-                source = source.replace(/'/g, "\\'");
-                source = source.replace(/"/g, "\\\"");
-                // remove basefolder we dont want to have in path
-                if (this.options.base) {
-                  filename = filename.substring(this.options.base.length, filename.length);
+            this.options.baseFolders.forEach(baseFolder => {
+              var files = glob.sync(baseFolder + "**/*.html", {});
+              for (var filename of files) {
+                if (filename.substr(-4) === 'html') {
+                  let fullpath = path.resolve(filename);
+                  // adds this file to being watched by webpack for a rebuild
+                  compilation.fileDependencies.add(fullpath);
+                  var source = fs.readFileSync(filename);
+                  source = source.toString();
+                  source = htmlMinifier.minify(source, Object.assign(defaultMinifyOptions, this.options.minifyOptions));
+                  source = source.replace(/\r?\n|\r/g, "");
+                  source = source.replace(/\\/g, "\\\\");
+                  source = source.replace(/'/g, "\\'");
+                  source = source.replace(/"/g, "\\\"");
+                  // remove basefolder we dont want to have in path
+                  if (this.options.base) {
+                    filename = filename.substring(this.options.base.length, filename.length);
+                  }
+                  // if we have a target put all the files directly there
+                  if (this.options.target) {
+                    filename = this.options.target + path.basename(filename);
+                  }
+                  filelist += (`$templateCache.put("${filename}", "${source}" ); \n`);
                 }
-                // if we have a target put all the files directly there
-                if (this.options.target) {
-                  filename = this.options.target + path.basename(filename);
-                }
-                filelist += (`$templateCache.put("${ filename }", "${ source }" ); \n`);
               }
-            }
+
+            });
 
             filelist += '}]);';
 
